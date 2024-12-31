c149f577709491ad1be57a0896653723835e4caf
Thupha41 <phatngo.040103@gmail.com>
2024-12-31 22:57:18 +0700

    Set up project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Drawer = createDrawerNavigator();
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function HomeScreen(props: any) {
+  console.log('>>> check props', props)
+  const {navigation} = props
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title='Go to detail' onPress={()=> navigation.navigate("Details")}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const AppRoute = () => {
+    return (
+        <View>
+            <Text>Hello Word with expo router</Text>
+
+        </View>
+
+    )
+}
+
+export default AppRoute
